a6ac7b9d9573041c11c0acd37302baaa2859c4ee
umasstr <trodriguez2011@gmail.com>
2019-08-23 11:16:56 -0400

    gs_I2.sh temp dir fix

--- src/gs_I2.sh
+++ src/gs_I2.sh
@@ -16,5 +16,5 @@ done
 #
 nohup bcl2fastq -r 1 -p $t -w 1 --runfolder-dir $r -o $o/temp_I2 --use-bases-mask Y1N*,N*,I*,N* --create-fastq-for-index-reads
 #
-python UMIbarcode_python2.py temp_I2/*I2*.gz > UMIs.txt
-rm -r temp_I2
+python UMIbarcode_python2.py $o/temp_I2/*I2*.gz > UMIs.txt
+rm -r $o/temp_I2
