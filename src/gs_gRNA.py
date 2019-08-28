03473fb18aa7baf6e9662fb16656babb2551f158
umasstr <trodriguez2011@gmail.com>
2019-08-28 10:33:12 -0400

    added gs_gRNA.py, updated workflow

---
+++ src/gs_gRNA.py
@@ -0,0 +1,4 @@
+#/usr/bin/env python
+import sys
+print '>' + sys.argv[1] + '\n' + sys.argv[2] 
+
--- src/gs_preprocess.sh
+++ src/gs_preprocess.sh
@@ -2,8 +2,7 @@
 echo "GUIDE-seq pre-processing for GHPCC"
 
 if [ "$1" == "-h" ]; then
-  echo "-t <number_of_threads> -o </path/to/output_directory> -r <directory_containing_RunInfo.xml> -s </path/to/SampleSheet.csv> -b </path/to/BWAIndex/genome.fa>"
-  echo "gs_preprocess output: output_dir/bam_files"
+  echo "-t <number_of_threads> -o </path/to/output_directory> -r <directory_containing_RunInfo.xml> -s </path/to/SampleSheet.csv> -b </path/to/BWAIndex/genome.fa> -g <gRNA_sequence>"
   echo "Required: 32G RAM"
   echo "Illumina-format SampleSheet: https://help.basespace.illumina.com/articles/descriptive/sample-sheet/"
   echo "RunInfo.xml: Contains high-level run information,such as the number of Reads and cycles in the sequencing run. Standard output from any illumina sequencer"
@@ -15,21 +14,23 @@ t=1
 o=fastq_output
 r=$PWD
 s=SampleSheet.csv
-b=Homo_sapiens/UCSC/hg38/Sequence/BWAIndex/genome.fa
 
-while getopts t:o:r:s:b: options; do
+while getopts t:o:r:s:b:g: options; do
 	case $options in 
 		t) t=$OPTARG;;
 		o) o=$OPTARG;;
 		r) r=$OPTARG;;
 		s) s=$OPTARG;;
 		b) b=$OPTARG;;
+		g) g=$OPTARG;;
 	esac
 done
 
 ./gs_demultiplex.sh -t $t -o $o -r $r -s $s
 
 ./gs_I2.sh -t $t -o $o -r $r
+
+python gs_gRNA.py gRNA $g > gRNA.fa
 
 python UMIbarcode_python2.py $o/temp_I2/*I2*.gz > UMIs.txt
 rm -rf $o/temp_I2
